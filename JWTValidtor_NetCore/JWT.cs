276e023a73fd3109464b5476b1249f42ff1850e9
begininvoke <zapezhman@gmail.com>
2020-08-14 13:31:35 +0430

    Add project files.

---
+++ JWTValidtor_NetCore/JWT.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jwt;
+using Microsoft.CodeAnalysis;
+namespace JWTValidtor_NetCore
+{
+
+   
+    class JWT
+    {
+        private static JWT instance = null;
+        public string JWTToken { get; set; }
+
+        List<string> listpasswords = new List<string>();
+		public string FileName { get; set; }
+		public JWT()
+        {
+
+        }
+        public static JWT Instance
+        {
+            get
+            {
+                if (instance==null)
+				{
+                    instance = new JWT();
+                }
+                return instance;
+            }
+
+        }
+
+		public void Worker()
+		{
+			Console.WriteLine("Github && twitter : @begininvoke");
+			listpasswords.AddRange(System.IO.File.ReadAllLines(FileName).ToList());
+			BruteForce(JWTToken);
+		}
+		private void BruteForce(string Token)
+		{
+			if (!string.IsNullOrEmpty(Token))
+			{
+				JWTToken =Token;
+				foreach (var item in listpasswords)
+				{
+					var ret = invalidnew(item);
+					if (ret)
+					{
+						Console.WriteLine( "Password Find " + item);
+						
+						return;
+					}
+					else
+						Console.WriteLine("Faild " + item);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Jwt is Empty!");
+				return;
+			}
+		}
+		public bool invalidnew(string secret)
+		{
+
+
+			try
+			{
+				string jsonPayload = Jwt.JsonWebToken.Decode(JWTToken, secret);
+
+				return true;
+			}
+			catch
+			{
+
+
+			}
+			return false;
+		}
+	}
+}
---
+++ JWTValidtor_NetCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JWTValidtor_NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length>0)
+            {
+
+         
+            if (string.IsNullOrEmpty(args[0]) || string.IsNullOrEmpty(args[1]))
+            {
+                Console.WriteLine("Value is empty ! See help ");
+                Console.WriteLine("dotnet JWTValidtor_NetCore.dll \"jwttoken\" PasswordList.txt ");
+                return;
+            }
+            if (args[0].Contains("-h"))
+            {
+                Console.WriteLine("dotnet JWTValidtor_NetCore.dll \"jwttoken\" PasswordList.txt ");
+                return;
+            }
+           
+            JWT jwt = JWT.Instance;
+            jwt.FileName = args[1];
+            jwt.JWTToken = args[0];
+            jwt.Worker();
+                Console.ReadKey();
+            }
+            else Console.WriteLine("Value is empty ! See help -h ");
+           
+        }
+    }
+}
